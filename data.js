52a2e8c7775fa6c22504730058e30a9ff137b667
Franirribarrat <franirribarrat@uc.cl>
2024-10-23 13:28:16 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,76 @@
+[
+    {
+        "nombre": "Libro Aves Lafkenche",
+        "descripcion": "Consiste en diseñar un libro de aves dirigido a niñas y niños desde 1° a 6° básico de las escuelas rurales de la comuna de Teodoro Schmidt. Todo el curso competía para que su diseño fuera seleccionado.",
+        "fecha": "2018",
+        "signo": "Editorial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_010.png?raw=true",
+        "fotos": ["aves-lafkenche.jpg", "aves-lafkenche-02", "aves-lafkenche-03"],
+        "github": "https://github.com/franirribarrat"
+
+    },
+    {
+        "nombre": "Tulilamp",
+        "fecha": "2022",
+        "signo": "Industrial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_09.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Revista Voces de la Araucanía",
+        "fecha": "2000-04-20",
+        "signo": "Editorial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_03.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Afiche Barrio Yungay",
+        "fecha": "2023",
+        "signo": "Editorial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_02.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Macro lilas",
+        "fecha": "2024",
+        "signo": "Fotografia",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_05.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Flamencos",
+        "fecha": "2023",
+        "signo": "Fotografia",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_06.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Banca",
+        "fecha": "2023",
+        "signo": "Industrial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_01.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Monograma",
+        "fecha": "2023",
+        "signo": "Editorial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_08.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Infografía",
+        "fecha": "2020",
+        "signo": "Editorial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_04.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    },
+    {
+        "nombre": "Cartel",
+        "fecha": "2023",
+        "signo": "Editorial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_07.png?raw=true",
+        "github": "https://github.com/franirribarrat"
+    }
+]
+
